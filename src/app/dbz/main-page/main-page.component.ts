f350208bd73815117c99210f5591598bd6702209
Angeline <joceline.angeline.de.luna.flores@ibm.com>
2023-04-13 14:27:07 -0700

    hola

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
 
 
 @Component({
@@ -7,15 +9,22 @@ import { Personaje } from '../interfaces/dbz.interface';
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-  personajes: Personaje [] = [
-    {
-      nombre:'Goku',
-      poder:15000
-    },
-    {
-      nombre: 'Vegeta',
-      poder:7500
-    }
-  ];
 
+  nuevo:Personaje = {
+    nombre: 'Maestro Roshi',
+    poder: 1000
+  }
+
+  get personajes(): Personaje [] {
+    return this.dbzService.personajes;
+  }
+
+  aggnuevopersonaje (argumento:Personaje) {
+    this.personajes.push(argumento);
+  }
+
+  constructor( private dbzService: DbzService) {
+
+  }
 }
+ 
